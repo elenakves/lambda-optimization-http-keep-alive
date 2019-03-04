e8ea662060d095084cd763a5cb4f0a746895bc8c
dottorsaimon <simone.zennaro@earthcloud.it>
2019-03-04 16:09:00 +0100

    removing comments

--- keepalive.js
+++ keepalive.js
@@ -45,7 +45,4 @@ module.exports.matricula = async (event, context) => {
       input: res
     }),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
